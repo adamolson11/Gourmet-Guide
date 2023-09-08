5029de5636dc35e1d661607b767b29526c3b8cfb
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-09-08 09:52:46 -0500

    added recipeData.json

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
 const User = require('./User');
+const Recipe= require('./Recipe')
 
-module.exports = { User };
+module.exports = { User, Recipe };
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,8 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Recipe } = require('../models');
 
 const userData = require('./userData.json');
+const recipeData = require('./recipeData.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -10,6 +11,11 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
+
+  await Recipe.bulkCreate(recipeData,{
+    individualHooks: true,
+    returning: true,
+  })
 
   process.exit(0);
 };
