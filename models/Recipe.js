067474e8e31746a4f23f28c048ab8bce0774ee78
Adam Olson <adam.lee.olson@outlook.com>
2023-09-07 21:23:24 -0500

    adding to recipe model

--- models/Recipe.js
+++ models/Recipe.js
@@ -4,25 +4,47 @@ const sequelize = require('../config/connection');
 class Recipe extends Model { }
 
 Recipe.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
 
     name: {
         type: DataTypes.STRING,
         allowNull: false,
 
-    }
+    },
 
+    description: {
+        type: DataTypes.STRING,
+      },
+
+      
+    //should ingredients and instructions be separate models? 
     // ingredients: {
     //     type: DataTypes.STRING,
+    //      allowNull: false,
+
+
 
     //chicken soup
     // meat, vegetable, spices, (all are arrays???)
     // spices
-        //pepper
-            //1 tsp
+    //pepper
+    //1 tsp
 
     //instructions: (array of steps objects)
 
     // },
+
+    date_created: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+    },
+
 
 
 })
--- models/User.js
+++ models/User.js
@@ -10,12 +10,12 @@ class User extends Model {
 
 User.init(
   {
-  //   id: {
-  //     type: DataTypes.INTEGER,
-  //     allowNull: false,
-  //     primaryKey: true,
-  //     autoIncrement: true,
-  //   },
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
 
     userName: {
       type: DataTypes.STRING,
