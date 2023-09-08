30b6687502d69ffa8a4812224aaf6dc8f02fcd71
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-09-08 11:12:20 -0500

    added notes

--- models/User.js
+++ models/User.js
@@ -46,9 +46,13 @@ User.init(
       },
     },
     // createdRecipesIds: {
-    //   type: DataTypes.JSON,
-    //   unique: true
-    // } we may not need this, as new users will not have any by default. It can be added when a user submits a new recipe, if we add that functionality
+    //   type: DataTypes.ARRAY(DataTypes.INTEGER),
+    // references: {
+    //   model: 'recipe',
+    //   key: 'id'
+    // }
+    // } 
+    //we may not need this, as new users will not have any by default. It can be added when a user submits a new recipe, if we add that functionality. We will need to make refernce table UserRecipes in order to use this
   },
   {
     hooks: {
