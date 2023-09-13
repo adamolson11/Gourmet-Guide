0e7c847b21d4ffb61f335f4f4ecb0d1e2d909852
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-09-12 19:46:45 -0500

    started newUserForm.js

---
+++ public/js/newUserForm.js
@@ -0,0 +1,27 @@
+const usernameEl = document.getElementById('username')
+const firstNameEl = document.getElementById('firstName')
+const lastNameEl = document.getElementById('lastName')
+const emailEl = document.getElementById('email')
+const passwordEl = document.getElementById('password')
+const formEl = document.querySelector('form')
+
+formEl.addEventListener('submit', e => {
+    e.preventDefault()
+
+    if (username.length<3 || username.length>25) {
+        alert("Username must be between 4 and 25 characters")
+    }
+
+    if (passwordEl.value.length<8 || passwordEl.value.length>128) {
+        alert("Password must be between 8 and 128 characters")
+    }
+
+    const newUserData = {
+        username: usernameEl.value,
+        firstName: firstNameEl.value,
+        lastName: lastNameEl.value,
+        email: emailEl.value,
+        password: passwordEl.value
+    }
+
+})
