59a9954fdb7a16a0a0ed95e107e2df2c5171b47d
noahr33 <128255532+noahr33@users.noreply.github.com>
2023-09-12 21:13:16 -0500

    createElement from btn

--- public/js/addRecipe.js
+++ public/js/addRecipe.js
@@ -1,26 +1,40 @@
 const ingredientBtn = document.getElementById('addIngredient')
-const ingredientName = document.getElementById('ingredientName')
-const ingredientAmount = document.getElementById('ingredientAmount')
-const ingredientInputs = document.getElementById('ingredientInputs')
+const stepsBtn = document.getElementById('stepsBtn')
 const nameDiv = document.getElementById('nameDiv')
 const amountDiv = document.getElementById('amountDiv')
-
-console.log("hello")
+const stepDiv = document.getElementById('stepDiv')
 
 ingredientBtn.addEventListener('click', (e) => {
-  // e.preventDefault()
+  e.preventDefault()
   const nameEl = document.createElement('input')
-  // const amountEl = document.createElement('input')
+  const amountEl = document.createElement('input')
 
   nameEl.setAttribute('type', 'text')
-  nameEl.classList.addClass('form-control col')
+  nameEl.classList.add('form-control')
+  nameEl.classList.add('col')
+  nameEl.classList.add('mt-1')
+
+  amountEl.setAttribute('type', 'text')
+  amountEl.classList.add('form-control')
+  amountEl.classList.add('col')
+  amountEl.classList.add('mt-1')
 
 
 
 
 
   nameDiv.appendChild(nameEl)
-  // amountDiv.appendChild(amountEl)
+  amountDiv.appendChild(amountEl)
 
 })
 
+stepsBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  const stepEl = document.createElement('input')
+
+  stepEl.setAttribute('type', 'text')
+  stepEl.classList.add('form-control')
+  stepEl.classList.add('mt-1')
+
+  stepDiv.appendChild(stepEl)
+})
