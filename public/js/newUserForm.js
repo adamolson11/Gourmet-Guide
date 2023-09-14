b71f5fd9a2db0f3917da65a77003caee4cadc080
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-09-14 09:01:48 -0500

    added catch for special characters in username

--- public/js/newUserForm.js
+++ public/js/newUserForm.js
@@ -5,16 +5,41 @@ const emailEl = document.getElementById('email')
 const passwordEl = document.getElementById('password')
 const formEl = document.querySelector('form')
 
+const specChar = /[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]/
+// /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?0-9]/
 formEl.addEventListener('submit', async e => {
     e.preventDefault()
+    // 
+    // const firstNameTest = /^[a-zA-Z]/.test(firstNameEl.value)
+    // const lastNameTest = /^[a-zA-Z]/.test(lastNameEl.value)
+    const usernameTest = specChar.test(usernameEl.value)
+    
 
+    console.log(usernameTest)
+
+    //check length fior username and password
     if (usernameEl.value.length<3 || usernameEl.value.length>25) {
         alert("Username must be between 4 and 25 characters")
+        return
     }
 
     if (passwordEl.value.length<8 || passwordEl.value.length>128) {
         alert("Password must be between 8 and 128 characters")
-    }
+        return
+        }
+
+    //     //checks if firstName and lastName are only letters 
+    // if (firstNameTest === false || lastNameTest === false) {
+    //     alert("Name can only be letters")
+    //     return
+    //  }
+    
+
+     if (usernameTest) {
+        alert("Username can only be letters and numbers")
+        return
+     }
+
 
     const newUserData = {
         username: usernameEl.value,
@@ -49,7 +74,7 @@ formEl.addEventListener('submit', async e => {
           req.json(response)
     }
     catch (err) {
-        console.log(err)
+        req.status(500).json(err)
     }
 
 })
